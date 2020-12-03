5347f9c3e63e10122e417c4dc28f2af85cf50375
Raphaël Thériault <raphael_theriault@outlook.com>
2020-12-03 10:26:15 -0500

    Day 3 - Improvements

--- src/d03p2.rs
+++ src/d03p2.rs
@@ -13,24 +13,20 @@ fn main() {
 
 fn run(input: Vec<Vec<bool>>) -> usize {
     let slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
-    let mut total = 0;
+    let mut total = 1;
 
     for (right, down) in slopes.iter().copied() {
-        let mut col = 0;
+        let mut col = right;
         let mut trees = 0;
 
-        for row in input.iter().step_by(down) {
+        for row in input.iter().step_by(down).skip(1) {
             if row[col] {
                 trees += 1;
             }
             col = (col + right) % row.len();
         }
 
-        if total > 0 {
-            total *= trees;
-        } else {
-            total = trees;
-        }
+        total *= trees;
     }
 
     total
